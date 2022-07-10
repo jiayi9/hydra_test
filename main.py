a5201b01298702278482424ebcfb2186ca3502ce
Jiayi Lu <215756347@qq.com>
2022-07-10 16:47:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,32 @@
 import hydra
-#from omegaconf import DictConfig, OmegaConf
-#import flatdict
-
-from dataclasses import dataclass
-from hydra.core.config_store import ConfigStore
-
-@dataclass
-class Files:
-    data: str
-    log: str
-    
-@dataclass
-class Params:
-    x: int
-    y: int
-    
-@dataclass
-class ThisConfig:
-    files: Files
-    params: Params
-
-cs = ConfigStore.instance()
-cs.store(name='xxx', node=ThisConfig)
+from omegaconf import DictConfig, OmegaConf
+import flatdict
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+from albumentations import (OneOf, Resize, Normalize, Compose, Transpose, HorizontalFlip, VerticalFlip,
+                            Cutout, ShiftScaleRotate, GaussianBlur, RandomBrightness, Sharpen, RandomCrop)
 
 @hydra.main(version_base=None, config_path="conf",config_name = "config")
-def main(cfg: ThisConfig):
-    print(cfg)
-    print(cfg.files)
-    print(cfg.files)
-    print(cfg.params)
+def main(cfg):
+    #print(cfg)
+
     print(type(cfg))
+    
+    transforms = []
+
+    for aug_cfg in cfg['train_aug']:
+        print(aug_cfg)
+        print(aug_cfg.trans_func_name)
+        params = OmegaConf.to_container(aug_cfg.params,
+                                        resolve=True)
+        print(params)
+        transforms.append(
+            getattr(A, aug_cfg.trans_func_name)(**params)
+        )
+
+    transforms.append(ToTensorV2())
+    
+    print(transforms)
     
     """
     x = OmegaConf.to_yaml(cfg)
